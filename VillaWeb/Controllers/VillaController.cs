6644ac162002b4c2d7bbf046797abdc61adf2ffb
bipin thapa <bipzzzlogin@gmail.com>
2024-12-14 02:00:03 +0545

    3.4.Added Villa controller

---
+++ VillaWeb/Controllers/VillaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db) {
+            _db= db;
+        }
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+    }
+}
