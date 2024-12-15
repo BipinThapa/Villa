59223084a6b4bb29e6447427436f973d2d36042c
bipin thapa <bipzzzlogin@gmail.com>
2024-12-15 23:12:07 +0545

    6.2.Create VillaNumber class and seed

--- Villa.Infrastructure/Data/ApplicationDbContext.cs
+++ Villa.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace Villa.Infrastructure.Data
         }
 
         public DbSet<Vila> Villas { get; set; }
+        public DbSet<VilaNumber> VillaNumbers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -51,6 +52,47 @@ namespace Villa.Infrastructure.Data
                       Sqft = 400
                   }
                 );
+            modelBuilder.Entity<VilaNumber>().HasData(
+                new VilaNumber
+                {
+                    Villa_Number=101,
+                    VillaId=1
+                },
+                new VilaNumber
+                {
+                    Villa_Number = 102,
+                    VillaId = 1
+                },
+                new VilaNumber
+                {
+                    Villa_Number = 103,
+                    VillaId = 1
+                },
+                new VilaNumber
+                {
+                    Villa_Number = 104,
+                    VillaId = 1
+                },
+                new VilaNumber
+                {
+                    Villa_Number = 201,
+                    VillaId = 2
+                },
+                new VilaNumber
+                {
+                    Villa_Number = 202,
+                    VillaId = 2
+                },
+                 new VilaNumber
+                 {
+                     Villa_Number = 203,
+                     VillaId = 2
+                 },
+                 new VilaNumber
+                 {
+                     Villa_Number = 301,
+                     VillaId = 3
+                 });
         }
     }
 }
