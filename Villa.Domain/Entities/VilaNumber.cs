7221493854b84664e71ec254ebb5ebd360568151
bipin thapa <bipzzzlogin@gmail.com>
2024-12-16 00:07:16 +0545

    6.6.ViewData and VillaNumber Class

--- Villa.Domain/Entities/VilaNumber.cs
+++ Villa.Domain/Entities/VilaNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,9 +12,11 @@ namespace Villa.Domain.Entities
     public class VilaNumber
     {
         [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Villa Number")]
         public int Villa_Number { get; set; }
         [ForeignKey("Villa")]
         public int VillaId { get; set; }
+        [ValidateNever]
         public Vila Villa { get; set; }
         public string? SpecialDetails { get; set; }
     }
---
+++ VillaWeb/Controllers/VillaNumberController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Villa.Domain.Entities;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.Include(x => x.Villa).ToList();
+            return View(villaNumbers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u => new SelectListItem {
+            Text=u.Name,
+            Value=u.Id.ToString()
+            });
+            ViewData["VillaList"] = list;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(VilaNumber obj)
+        {
+            ModelState.Remove("Villa");
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The Villa Number has been created successfully.";
+                return RedirectToAction(nameof(Index), "VillaNumber");
+            }
+            return View(obj);
+        }
+    }
+}
